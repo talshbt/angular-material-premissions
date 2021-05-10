bb4131b116f29fe3614e2dcc062ebe6dbdc26b01
talshbt <talshbt@gmail.com>
2021-05-10 16:48:27 +0300

    6 files added

---
+++ src/app/multi-select-search/multi-select-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-select-search',
+  templateUrl: './multi-select-search.component.html',
+  styleUrls: ['./multi-select-search.component.css']
+})
+export class MultiSelectSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/tree-dynamic/tree-dynamic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tree-dynamic',
+  templateUrl: './tree-dynamic.component.html',
+  styleUrls: ['./tree-dynamic.component.css']
+})
+export class TreeDynamicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
