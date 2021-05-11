bc41c331f18c7954250760a0a4a8af829310f4c2
talshbt <talshbt@gmail.com>
2021-05-11 10:34:03 +0300

    1 file modified, 2 added

---
+++ src/app/tree-dynamic/dynamic-data-base.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { DynamicFlatNode } from "./dynamic-flat-node";
+
+@Injectable({ providedIn: 'root' })
+export class DynamicDatabase {
+  dataMap = new Map<string, string[]>([
+    ['Tribe', ['Apple', 'Orange', 'Banana']],
+    ['Mesila', ['Tomato', 'Potato', 'Onion']],
+    ['Rest', ['Fuji', 'Macintosh']],
+    ['Web Client', ['Yellow', 'White', 'Purple']]
+  ]);
+
+
+  rootLevelNodes: string[] = ['Tribe', 'Mesila'];
+
+  /** Initial data from database */
+  initialData(): DynamicFlatNode[] {
+    return this.rootLevelNodes.map(name => new DynamicFlatNode(name, 0, true));
+  }
+
+  getChildren(node: string): string[] | undefined {
+    return this.dataMap.get(node);
+  }
+
+  isExpandable(node: string): boolean {
+    return this.dataMap.has(node);
+  }
+}
--- src/app/tree-dynamic/tree-dynamic.component.ts
+++ src/app/tree-dynamic/tree-dynamic.component.ts
@@ -7,46 +7,16 @@ import { FlatTreeControl } from '@angular/cdk/tree';
 import { Component, Injectable } from '@angular/core';
 import { BehaviorSubject, merge, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { DynamicDatabase } from './dynamic-data-base';
+import { DynamicFlatNode } from './dynamic-flat-node';
 
 /** Flat node with expandable and level information */
-export class DynamicFlatNode {
-  constructor(
-    public item: string,
-    public level = 1,
-    public expandable = false,
-    public isLoading = false
-  ) {}
-}
 
 /**
  * Database for dynamic data. When expanding a node in the tree, the data source will need to fetch
  * the descendants data from the database.
  */
-@Injectable({ providedIn: 'root' })
-export class DynamicDatabase {
-  dataMap = new Map<string, string[]>([
-    ['Tribe', ['Apple', 'Orange', 'Banana']],
-    ['Mesila', ['Tomato', 'Potato', 'Onion']],
-    ['Rest', ['Fuji', 'Macintosh']],
-    ['Web Client', ['Yellow', 'White', 'Purple']]
-  ]);
 
-
-  rootLevelNodes: string[] = ['Tribe', 'Mesila'];
-
-  /** Initial data from database */
-  initialData(): DynamicFlatNode[] {
-    return this.rootLevelNodes.map(name => new DynamicFlatNode(name, 0, true));
-  }
-
-  getChildren(node: string): string[] | undefined {
-    return this.dataMap.get(node);
-  }
-
-  isExpandable(node: string): boolean {
-    return this.dataMap.has(node);
-  }
-}
 /**
  * File database, it can build a tree structured Json object from string.
  * Each node in Json object represents a file or a directory. For a file, it has filename and type.
@@ -140,6 +110,15 @@ export class DynamicDataSource implements DataSource<DynamicFlatNode> {
     }, 1000);
   }
 }
+
+
+
+
+
+
+
+
+
 
 /**
  * @title Tree with dynamic data
