d6262956da76b24aec9056be9b9590d02f50ea33
talshbt <talshbt@gmail.com>
2021-05-20 15:13:52 +0300

    1 file modified

--- src/app/tree-checklist/todo-Item-node.ts
+++ src/app/tree-checklist/todo-Item-node.ts
@@ -1,9 +1,12 @@
-import { Injectable, OnInit } from '@angular/core';
-import { PermissionService } from '../permission.service';
+import { Injectable } from '@angular/core';
 
-@Injectable({ providedIn: 'root' })
-export class TodoItemNode implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+// @Injectable({ providedIn: 'root' })
+// export class TodoItemNode implements OnInit {
+//   ngOnInit(): void {
+//     throw new Error('Method not implemented.');
+//   }
+// }
+export class TodoItemNode {
+  children: TodoItemNode[];
+  item: string;
 }
