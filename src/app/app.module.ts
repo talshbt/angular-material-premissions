523b69e770c1ff415459d89ed6dd5ec29f3e55ea
talshbt <talshbt@gmail.com>
2021-05-20 15:45:12 +0300

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { MultiSelectSearchComponent } from './multi-select-search/multi-select-search.component';
 import { TreeDynamicComponent } from './tree-dynamic/tree-dynamic.component';
+import { TreeChecklistExampleComponent } from './tree-checklist-example/tree-checklist-example.component';
 
 @NgModule({
   imports: [
@@ -23,8 +24,15 @@ import { TreeDynamicComponent } from './tree-dynamic/tree-dynamic.component';
     ReactiveFormsModule,
     NgxMatSelectSearchModule,
     MaterialModule
-    ],
-  declarations: [AppComponent, MultiSelectSearchComponent, TreeDynamicComponent],
+  ],
+  declarations: [
+    AppComponent,
+    MultiSelectSearchComponent,
+    TreeDynamicComponent,
+    TreeChecklistExampleComponent
+  ],
+
+  entryComponents: [TreeChecklistExampleComponent],
   bootstrap: [AppComponent],
   providers: []
 })
--- src/app/tree-checklist-example/tree-checklist-example.component.ts
+++ src/app/tree-checklist-example/tree-checklist-example.component.ts
@@ -1,19 +1,103 @@
-import { SelectionModel } from '@angular/cdk/collections';
-import { FlatTreeControl } from '@angular/cdk/tree';
-import { Component, OnInit } from '@angular/core';
-import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
-import { ChecklistDatabase } from './check-list-database';
-import { TodoItemFlatNode } from './todo-item-flat-node';
-import { TodoItemNode } from './todo-item-node';
+import {SelectionModel} from '@angular/cdk/collections';
+import {FlatTreeControl} from '@angular/cdk/tree';
+import {Component, Injectable} from '@angular/core';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import {BehaviorSubject} from 'rxjs';
+
+export class TodoItemNode {
+  children: TodoItemNode[];
+  item: string;
+}
+
+/** Flat to-do item node with expandable and level information */
+export class TodoItemFlatNode {
+  item: string;
+  level: number;
+  expandable: boolean;
+}
+
+/**
+ * The Json object for to-do list data.
+ */
+const TREE_DATA = {
+  Groceries: {
+    'Almond Meal flour': null,
+    'Organic eggs': null,
+    'Protein Powder': null,
+    Fruits: {
+      Apple: null,
+      Berries: ['Blueberry', 'Raspberry'],
+      Orange: null
+    }
+  },
+  Reminders: [
+    'Cook dinner',
+    'Read the Material Design spec',
+    'Upgrade Application to Angular'
+  ]
+};
+
+/**
+ * Checklist database, it can build a tree structured Json object.
+ * Each node in Json object represents a to-do item or a category.
+ * If a node is a category, it has children items and new items can be added under the category.
+ */
+@Injectable()
+export class ChecklistDatabase {
+  dataChange = new BehaviorSubject<TodoItemNode[]>([]);
+
+  get data(): TodoItemNode[] { return this.dataChange.value; }
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize() {
+    // Build the tree nodes from Json object. The result is a list of `TodoItemNode` with nested
+    //     file node as children.
+    const data = this.buildFileTree(TREE_DATA, 0);
+
+    // Notify the change.
+    this.dataChange.next(data);
+  }
+
+  /**
+   * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
+   * The return value is the list of `TodoItemNode`.
+   */
+  buildFileTree(obj: {[key: string]: any}, level: number): TodoItemNode[] {
+    return Object.keys(obj).reduce<TodoItemNode[]>((accumulator, key) => {
+      const value = obj[key];
+      const node = new TodoItemNode();
+      node.item = key;
+
+      if (value != null) {
+        if (typeof value === 'object') {
+          node.children = this.buildFileTree(value, level + 1);
+        } else {
+          node.item = value;
+        }
+      }
+
+      return accumulator.concat(node);
+    }, []);
+  }
+}
+
 
 @Component({
-  selector: 'app-tree-checklist',
-  templateUrl: './tree-checklist.component.html',
-  styleUrls: ['./tree-checklist.component.css'],
+  selector: 'app-tree-checklist-example',
+  templateUrl: './tree-checklist-example.component.html',
+  styleUrls: ['./tree-checklist-example.component.css'],
   providers: [ChecklistDatabase]
-
 })
-export class TreeChecklistComponent  {
+
+
+
+export class TreeChecklistExampleComponent{
+
+
+
 
   /** Map from flat node to nested node. This helps us finding the nested node to be modified */
   flatNodeMap = new Map<TodoItemFlatNode, TodoItemNode>();
@@ -150,5 +234,18 @@ export class TreeChecklistComponent  {
     }
     return null;
   }
+
+  /** Select the category so we can insert the new item. */
+  // addNewItem(node: TodoItemFlatNode) {
+  //   const parentNode = this.flatNodeMap.get(node);
+  //   this._database.insertItem(parentNode!, '');
+  //   this.treeControl.expand(node);
+  // }
+
+  // /** Save the node to database */
+  // saveNode(node: TodoItemFlatNode, itemValue: string) {
+  //   const nestedNode = this.flatNodeMap.get(node);
+  //   this._database.updateItem(nestedNode!, itemValue);
+  // }
 
 }
