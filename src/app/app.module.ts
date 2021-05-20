cf128097f9f96350b63730e3d11106d1dda96a88
talshbt <talshbt@gmail.com>
2021-05-20 16:01:27 +0300

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
 import { MultiSelectSearchComponent } from './multi-select-search/multi-select-search.component';
 import { TreeDynamicComponent } from './tree-dynamic/tree-dynamic.component';
 import { TreeChecklistExampleComponent } from './tree-checklist-example/tree-checklist-example.component';
+import { TreeChecklistComponent } from './tree-checklist/tree-checklist.component';
 
 @NgModule({
   imports: [
@@ -29,10 +30,11 @@ import { TreeChecklistExampleComponent } from './tree-checklist-example/tree-che
     AppComponent,
     MultiSelectSearchComponent,
     TreeDynamicComponent,
-    TreeChecklistExampleComponent
+    TreeChecklistExampleComponent,
+    TreeChecklistComponent
   ],
 
-  entryComponents: [TreeChecklistExampleComponent],
+  entryComponents: [TreeChecklistExampleComponent, TreeChecklistComponent],
   bootstrap: [AppComponent],
   providers: []
 })
--- src/app/tree-checklist/todo-item-node.ts
+++ src/app/tree-checklist/todo-item-node.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 // @Injectable({ providedIn: 'root' })
-export interface TodoItemNode {
+export class TodoItemNode {
   children: TodoItemNode[];
   item: string;
 }
