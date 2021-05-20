fb94e0fbcab287c6ad197c54a28eac4921662603
talshbt <talshbt@gmail.com>
2021-05-20 15:05:33 +0300

    5 files added

---
+++ src/app/tree-checklist/tree-checklist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tree-checklist',
+  templateUrl: './tree-checklist.component.html',
+  styleUrls: ['./tree-checklist.component.css']
+})
+export class TreeChecklistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
