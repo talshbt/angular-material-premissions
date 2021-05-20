86b650e422d59b78738e469d4541bc265cd4e889
talshbt <talshbt@gmail.com>
2021-05-20 15:09:11 +0300

    3 files modified

---
+++ src/app/tree-checklist/check-list-database.ts
@@ -0,0 +1,9 @@
+import { Injectable, OnInit } from '@angular/core';
+import { PermissionService } from '../permission.service';
+
+@Injectable({ providedIn: 'root' })
+export class CheckListDatabase implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
---
+++ src/app/tree-checklist/todo-Item-node.ts
@@ -0,0 +1,9 @@
+import { Injectable, OnInit } from '@angular/core';
+import { PermissionService } from '../permission.service';
+
+@Injectable({ providedIn: 'root' })
+export class TodoItemNode implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
